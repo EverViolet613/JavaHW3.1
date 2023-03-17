637fb322af3fd8c9f5558ddb8c48b37a8ce2a057
Yuliya Prokhorova <lostghost9751@gmail.com>
2023-03-17 13:00:35 +0300

    add Java project

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate (price);
+        System.out.println(miles);
+
+        System.out.println();
+        System.out.println("30_000");
+        System.out.println(service.calculate(30_000));
+
+        System.out.println();
+        System.out.println("50_200");
+        System.out.println(service.calculate(50_200));
+
+        System.out.println();
+        System.out.println("301_000");
+        System.out.println(service.calculate(301_000));
+
     }
 }
